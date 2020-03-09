6b5d3286840047b96cb4344a81b0330b37f77f42
feige <feige84@126.com>
2020-03-09 14:40:54 +0800

    简单的拼接sql

--- builder_test.go
+++ builder_test.go
@@ -7,7 +7,9 @@ import (
 
 func TestBuilder(t *testing.T) {
 	s := &SelectSql{}
-	str, args := s.Table("v_user").Select("*").Where("user_nickname LIKE ?", "%哈哈%").Order("user_id DESC, user_reg_time DESC").Limit(10).Offset(10).BuildSql()
+	s.Table("v_user").Select("*")
+	s.Where("user_id=?", 1)
+	str, args := s.Where("user_nickname LIKE ?", "%哈哈%").Order("user_id DESC, user_reg_time DESC").Limit(10).Offset(10).BuildSql()
 	fmt.Println(str)
 	fmt.Println(args)
 }
